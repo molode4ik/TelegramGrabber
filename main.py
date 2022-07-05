47cd9458d72f80906b7aa55a114fc9b13432f730
molode4ik <91955445+molode4ik@users.noreply.github.com>
2022-07-05 11:16:56 +0300

    Update for grabber
    Script are creating the  new folder for every dialog and throws there the medias

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import asyncio
 import json
-import telethon
+import os
 from telethon import TelegramClient
 
 
@@ -11,53 +11,63 @@ def load_config(path='config.ini'):
 
     return int(config['TELEGRAM']['api_id']), str(config['TELEGRAM']['api_hash']) 
 
- 
+#Get all dialogs
+#Saving name of each dialog to json
 async def save_user_dialogs(client, dialogs_list):
     dialogs = {}
     for dialog in dialogs_list:
-        dialogs.update({dialog.name : dialog.entity.id})
+        if dialog.name != "":
+            dialogs.update({dialog.entity.id : dialog.name})
+        else:
+            dialogs.update({dialog.entity.id : dialog.entity.id})
 
     with open('dialogs_names.json', 'w', encoding='utf-8') as file:
         file.write(json.dumps(dialogs, ensure_ascii=False))
+#In json you can leave those channels whose data we need
 
 
 async def main():
-    api_id, api_hash = load_config('config.ini')
+    try:
+        api_id, api_hash, phone = load_config('config.ini')
+    except:
+        print('You dont have a config file\nCreate it before working')
     #Log in Telegram
-    client = TelegramClient("Test", api_id, api_hash) 
+    client = TelegramClient("Telegram", api_id, api_hash) 
     # Start Telegram client
     await client.start()  
-    #try:
-    with open('dialogs_names.json', encoding="utf8") as file:
-        chosed_dialogs = json.load(file)
-    await parse_data(client, chosed_dialogs)
-    #except Exception as ex:
-        #Get all dialogs
-        #Saving name of each dialog to jsoncle
-        #In json you can leave those channels whose data we need
-    #dialogs_list = await client.get_dialogs() 
-        #await save_user_dialogs(client, dialogs_list)
+    try:
+        with open('dialogs_names.json', encoding="utf8") as file:
+            chosed_dialogs = json.load(file)
+    except:
+        dialogs_list = await client.get_dialogs()
+        await save_user_dialogs(client, dialogs_list)   
     
-
+    await parse_data(client, chosed_dialogs)
+    
+        
+   
+#Parcing data from chosed dialogs
+#Save messages and media to folder of channel
 async def parse_data(client, chosed_dialogs):
     print('Parsing')
-    data = {}
+    path_to_folder = os.path.abspath('Telegram.session')
+    main_dir = path_to_folder.split('Telegram.session')[0]
     await client.get_dialogs() 
-    for id in chosed_dialogs.values():
-        messages = []
-        async for message in client.iter_messages(id):
+    for id in chosed_dialogs.keys():
+        data = {}
+        #os.mkdir(f"{main_dir}Media\\")
+        os.mkdir(f"{main_dir}Media\\{chosed_dialogs.get(id)}\\")
+        async for message in client.iter_messages(int(id)):
             if message.media is None:
-                messages.append(message.text)
+                data.update({str(message.date).split('+')[0] : message.text})
             elif message.file is not None:
-                await client.download_media(message = message.media, file=f"Media\\{id}\\{str(message.file.name)}")
+                await client.download_media(message = message.media, file=f"Media\\{chosed_dialogs.get(id)}\\{str(message.file.name)}")
         
-                
-        data.update({id : messages})
+        with open(f"Media\\{chosed_dialogs.get(id)}\\messages.json", 'w', encoding='utf-8') as file:
+            file.write(json.dumps(data, ensure_ascii=False))
 
     print('All is done')
-    with open("data.json", 'w', encoding='utf-8') as file:
-        file.write(json.dumps(data, ensure_ascii=False))
-
+    
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
